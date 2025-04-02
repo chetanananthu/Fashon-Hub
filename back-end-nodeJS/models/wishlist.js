ae815fe736a98e5c7e5f79a6c8a07d52d121bd56
Chetan Ananthu(UST <chetanananthu@gmail.com>
2025-04-02 12:45:21 +0530

    Added .gitkeep to track empty frontend folder

--- back-end-nodeJS/models/wishlist.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const WishlistSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
-  productIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }]
-});
-
-module.exports = mongoose.model("Wishlist", WishlistSchema);
